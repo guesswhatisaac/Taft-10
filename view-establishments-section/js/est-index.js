e257dd2d207b08c643d257cad2b23064b6babbcb
sheyn <127822959+shaneclm@users.noreply.github.com>
2024-03-06 12:38:06 +0800

    cleaned files under view-establishment-section

---
+++ view-establishments-section/js/est-index.js
@@ -0,0 +1,143 @@
+
+function changeText(option) {
+    var selectedText = option.textContent;
+    var titleElement = document.querySelector('.category-dropdown-title');
+    titleElement.textContent = selectedText;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const priceButtons = document.querySelectorAll('.price-button-inner, .price-button-outer-left, .price-button-outer-right');
+
+    priceButtons.forEach(button => {
+        button.addEventListener('click', togglePriceButtonClick);
+    });
+    
+    function togglePriceButtonClick(event) {
+        event.target.classList.toggle('price-button-clicked');
+        event.target.classList.toggle('price-button-outer-right.price-button-clicked');
+        event.target.classList.toggle('price-button-outer-left.price-button-clicked');
+    
+    }
+    
+    const input = document.querySelector('#tag-input');
+    const tagForm = document.querySelector('#tagForm');
+    const output = document.querySelector('.tags');
+    const max = document.querySelector('.max');
+    
+    function outputTag(tagValue) {
+        const tag = `
+            <span class="tag">
+                <span class="material-icons-outlined remove-button">
+                    x
+                </span>
+                <b>${tagValue}</b>
+            </span>
+        `;
+    
+        output.innerHTML += tag;
+    }
+    
+    tagForm.addEventListener('submit', e => {
+        e.preventDefault(); 
+    
+        if (input.value === "") {
+            return; 
+        }
+    
+        if (output.children.length >= 4) {
+            input.disabled = true;
+            input.placeholder = "Max tags reached!";
+            return;
+        }
+    
+        const tagValue = input.value.trim();
+        outputTag(tagValue); 
+        input.value = "";
+    });
+    
+    input.addEventListener('input', e => {
+        const sanitizedValue = input.value.replace(/[^\w]/g, ""); 
+        input.value = sanitizedValue;
+    });
+    
+    window.addEventListener('click', e => {
+        if (e.target.classList.contains('remove-button')) {
+            e.target.parentElement.remove();
+            input.disabled = false;
+            input.placeholder = "Add a tag...";
+        }
+    });
+
+    /********************** ADD REVIEWS **********************/ 
+    
+    document.querySelectorAll('.est-add-review').forEach(function(button) {
+        button.addEventListener('click', function () {
+            document.getElementById('reviewWindow').style.display = 'flex';
+        });
+    });
+    
+    document.querySelector('.review-window-container .close-button').addEventListener('click', function () {
+        document.getElementById('reviewWindow').style.display = 'none';
+    });
+
+    /********************** VIEW REVIEWS **********************/ 
+    
+    document.querySelectorAll('.est-view-review-24chicken').forEach(function(button) {
+        button.addEventListener('click', function () {
+            document.getElementById('view-reviewWindow-24chicken').style.display = 'flex';
+        });
+    });
+    
+    document.querySelector('.view-window-container-24chicken .close-button').addEventListener('click', function () {
+        document.getElementById('view-reviewWindow-24chicken').style.display = 'none';
+    });
+    
+    document.querySelectorAll('.est-view-review-ateRicas').forEach(function(button) {
+    
+        button.addEventListener('click', function () {
+            document.getElementById('view-reviewWindow-ateRicas').style.display = 'flex';
+        });
+    });
+    
+    document.querySelector('.view-window-container-ateRicas .close-button').addEventListener('click', function () {
+        document.getElementById('view-reviewWindow-ateRicas').style.display = 'none';
+    });
+    
+    document.querySelectorAll('.est-view-review-tomo').forEach(function(button) {
+    
+        button.addEventListener('click', function () {
+            document.getElementById('view-reviewWindow-tomo').style.display = 'flex';
+        });
+    });
+    
+    document.querySelector('.view-window-container-tomo .close-button').addEventListener('click', function () {
+        document.getElementById('view-reviewWindow-tomo').style.display = 'none';
+    });
+    
+    document.querySelectorAll('.est-view-review-tinuhog').forEach(function(button) {
+    
+        button.addEventListener('click', function () {
+            document.getElementById('view-reviewWindow-tinuhog').style.display = 'flex';
+        });
+    });
+    
+    document.querySelector('.view-window-container-tinuhog .close-button').addEventListener('click', function () {
+        document.getElementById('view-reviewWindow-tinuhog').style.display = 'none';
+    });
+    
+    document.querySelectorAll('.est-view-review-hungry-seoul').forEach(function(button) {
+    
+        button.addEventListener('click', function () {
+            document.getElementById('view-reviewWindow-hungry-seoul').style.display = 'flex';
+        });
+    });
+    
+    document.querySelector('.view-window-container-hungry-seoul .close-button').addEventListener('click', function () {
+        document.getElementById('view-reviewWindow-hungry-seoul').style.display = 'none';
+    });
+  });
+
+
+
+
