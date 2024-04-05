80451c514dbbeff5753c54f30b2c403dc5f0d084
sheyn <127822959+shaneclm@users.noreply.github.com>
2024-04-05 22:10:52 +0800

    updated

--- public/view-establishments-section/js/est-index.js
+++ public/view-establishments-section/js/est-index.js
@@ -80,64 +80,12 @@ const Establishment2 = function(name, priceRange, tags, description, coverImage)
   this.addReviewClass = generateAddReviewClass(name);
 };
 
-const establishments = [
-// {
-//   name: '24 Chicken',
-//   rating: '4.9',
-//   priceRange: ['₱₱', '₱₱'],
-//   tags: ['Filipino', 'Chicken'],
-//   description: 'If you\'re on the hunt for a chicken experience that transcends the ordinary, look no further than 24 Chicken.',
-//   coverImage: '24Chicken.png',
-//   reviewsButtonClass: 'est-view-review-24-chicken view-review-btn',
-//   addReviewClass: 'add-review-24-chicken add-review-btn'
-// },
-// {
-//   name: "Ate Rica's Bacsilog",
-//   rating: '4.9',
-//   priceRange: ['₱', '₱₱₱'],
-//   tags: ['Filipino', 'Rice Meal'],
-//   description: 'Ate Rica\'s Bacsilog lives up to its "Sauce Sarap" promise! Delicious, affordable Filipino comfort food with generous portions and...',
-//   coverImage: 'AteRicasBacsilog.png',
-//   reviewsButtonClass: 'est-view-review-ate-ricas-bacsilog view-review-btn',
-//   addReviewClass: 'add-review-ate-ricas-bacsilog add-review-btn'
-// },
-// {
-//   name: 'Tomo Coffee',
-//   rating: '4.7',
-//   priceRange: ['₱₱', '₱₱'],
-//   tags: ['Drinks'],
-//   description: 'Tucked away in a vibrant student district, Tomo Coffee is a haven for caffeine-craving scholars. I love it so much!',
-//   coverImage: 'TomoCoffee.png',
-//   reviewsButtonClass: 'est-view-review-tomo-coffee view-review-btn',
-//   addReviewClass: 'add-review-tomo-coffee add-review-btn'
-// },
-// {
-//   name: 'Tinuhog ni Benny',
-//   rating: '5.0',
-//   priceRange: ['₱', '₱₱₱'],
-//   tags: ['Filipino', 'Rice Meal'],
-//   description: 'Tinuhog ni Benny is a haven for budget-friendly, delicious Filipino comfort food. The highlight is undoubtedly their namesake "tinuhog"...',
-//   coverImage: 'TinuhogNiBenny.png',
-//   reviewsButtonClass: 'est-view-review-tinuhog-ni-benny view-review-btn',
-//   addReviewClass: 'add-review-tinuhog-ni-benny add-review-btn'
-// },
-// {
-//   name: 'Hungry Seoul',
-//   rating: '4.9',
-//   priceRange: ['₱₱', '₱₱'],
-//   tags: ['Korean', 'Rice Meal'],
-//   description: 'If you\'re craving a taste of Korea in Manila, Hungry Seoul is definitely worth a visit. This casual restaurant...',
-//   coverImage: 'HungrySeoul.png',
-//   reviewsButtonClass: 'est-view-review-hungry-seoul view-review-btn',
-//   addReviewClass: 'add-review-hungry-seoul add-review-btn'
-// },
-];
+const establishments = [];
 
 establishmentList = establishments;
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  /*
   fetch('/load-establishments', {
     method: 'GET',
     headers: {
@@ -176,7 +124,6 @@ document.addEventListener('DOMContentLoaded', function() {
       console.error('Error:', error);
       alert('Error getting data from server');
   });
-*/
 
 /*****************************    VIEW / HIDE MODAL    ****************************/
 
